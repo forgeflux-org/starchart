e9deb45e046115118837fc4626b31e7e66816395
realaravinth <realaravinth@batsense.net>
2022-04-30 18:31:01 +0000

    Deploying to gh-pages from  @ 3d981492441fec8e80e8cb7385b02f8537641f34 🚀

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["starchart"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"starchart/gitea/struct.User.html\" title=\"struct starchart::gitea::User\">User</a>","synthetic":false,"types":["starchart::gitea::User"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["starchart"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"starchart/gitea/struct.User.html\" title=\"struct starchart::gitea::User\">User</a>","synthetic":false,"types":["starchart::gitea::User"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
