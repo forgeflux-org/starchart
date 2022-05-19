d247da75d2f77a48b948359ef193b2ec3469b58e
realaravinth <realaravinth@batsense.net>
2022-05-19 16:23:09 +0000

    Deploying to gh-pages from  @ 0586f2e670200e1b2bc8a3afa3b303315d89b0d2 🚀

--- implementors/core/marker/trait.StructuralEq.js
+++ implementors/core/marker/trait.StructuralEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["gitea"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"gitea/schema/struct.User.html\" title=\"struct gitea::schema::User\">User</a>","synthetic":false,"types":["gitea::schema::User"]}];
+implementors["gitea"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"gitea/schema/struct.User.html\" title=\"struct gitea::schema::User\">User</a>","synthetic":false,"types":["gitea::schema::User"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
