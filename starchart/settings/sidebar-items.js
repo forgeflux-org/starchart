6c1b7ab310f425f5fdcb31ceb2a7f2ea27654cc1
realaravinth <realaravinth@batsense.net>
2022-05-26 17:54:51 +0000

    Deploying to gh-pages from  @ 036a90e74ac292b46738e15e3c562e9406915f4d 🚀

--- starchart/settings/sidebar-items.js
+++ starchart/settings/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["DBType",""],["LogLevel",""]],"struct":[["Crawler",""],["Database",""],["Repository",""],["Server",""],["Settings",""]]});
+initSidebarItems({"constant":[["CC0_LICENSE_TXT",""],["LICENSE_FILE",""]],"enum":[["DBType",""],["LogLevel",""]],"struct":[["Crawler",""],["Database",""],["Repository",""],["Server",""],["Settings",""]]});
